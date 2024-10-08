44722a5d732ef5b96e96f6e485eb9b99fa07d53d
Samuel Adams <sama29571@gmail.com>
2024-10-08 21:45:13 +0000

    save dataset

--- dataset.py
+++ dataset.py
@@ -13,16 +13,16 @@ mp_drawing_styles = mp.solutions.drawing_styles
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 # static_image_mode=True: It processes each image independently (static mode), which is suited for handling individual hand images.
 
-DATA_DIR = './data'
+DATA_DIR = '../data'
 
 data = []
 labels = []
 for class_dir in os.listdir(DATA_DIR):
     for img_path in os.listdir(os.path.join(DATA_DIR, class_dir)):
-        data_aux = []
+        normalised_cors = []
 
-        x_ = []
-        y_ = []
+        x_cors = []
+        y_cors = []
 
 
         img = cv2.imread(os.path.join(DATA_DIR, class_dir, img_path))
@@ -32,22 +32,30 @@ for class_dir in os.listdir(DATA_DIR):
         results = hands.process(img_rgb)
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
+                # mp_drawing.draw_landmarks(img_rgb,hand_landmarks,mp_hands.HAND_CONNECTIONS,mp_drawing_styles.get_default_hand_landmarks_style(),mp_drawing_styles.get_default_hand_connections_style())
+                
                 for i in range(len(hand_landmarks.landmark)):
                     x = hand_landmarks.landmark[i].x
                     y = hand_landmarks.landmark[i].y
 
-                    x_.append(x)
-                    y_.append(y)
+                    x_cors.append(x)
+                    y_cors.append(y)
 
                 for i in range(len(hand_landmarks.landmark)):
                     x = hand_landmarks.landmark[i].x
                     y = hand_landmarks.landmark[i].y
-                    data_aux.append(x - min(x_))
-                    data_aux.append(y - min(y_))
+                    normalised_cors.append(x - min(x_cors))
+                    normalised_cors.append(y - min(y_cors))
 
-            data.append(data_aux)
+            #the dataset then will comprise coordinates of the landmarks of the hand to predict the gesture
+            data.append(normalised_cors)
+
             labels.append(class_dir)
+        # plt.figure()
 
-# f = open('data.pickle', 'wb')
-# pickle.dump({'data': data, 'labels': labels}, f)
-# f.close()
+        # plt.imshow(img_rgb)
+        # plt.show()
+
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
